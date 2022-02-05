6389fee318e893254081c8947e0bdc6dc81973b7
Hasan Abo-Shally <HasanAboShally@gmail.com>
2022-02-05 23:23:37 +0200

    Update status-messages.ts

--- src/app/pages/group/group-dashboard/status-messages.ts
+++ src/app/pages/group/group-dashboard/status-messages.ts
@@ -1,115 +1,125 @@
-import { KHITMA_GROUP_TYPE } from "src/app/entities/entities";
-
+import { KHITMA_GROUP_TYPE } from 'src/app/entities/entities';
 
 function _getDateInArabic(date: Date) {
-    // var months = ["يناير", "فبراير", "مارس", "إبريل", "مايو", "يونيو",
-    //   "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
+  // var months = ["يناير", "فبراير", "مارس", "إبريل", "مايو", "يونيو",
+  //   "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
 
-    var days = ["اﻷحد", "اﻷثنين", "الثلاثاء", "اﻷربعاء", "الخميس", "الجمعة", "السبت"];
+  var days = [
+    'اﻷحد',
+    'اﻷثنين',
+    'الثلاثاء',
+    'اﻷربعاء',
+    'الخميس',
+    'الجمعة',
+    'السبت',
+  ];
 
-    return days[date.getDay()] + " " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getUTCFullYear();
+  return (
+    days[date.getDay()] +
+    ' ' +
+    date.getDate() +
+    '/' +
+    (date.getMonth() + 1) +
+    '/' +
+    date.getUTCFullYear()
+  );
 }
 
 function statusMsg_Sequential(group) {
+  function getJuzIcon(juz) {
+    const ICONS = ['🔴', '🟡', '🟢'];
 
-    function getJuzIcon(juz) {
+    return ICONS[juz.status];
+  }
 
-        const ICONS = ['🔴', '🟡', '🟢'];
+  const NEW_LINE = '\n';
+  const now = new Date();
 
-        return ICONS[juz.status];
-    }
+  let msg = group.title;
 
+  msg += NEW_LINE;
+  msg += _getDateInArabic(now);
+  msg += NEW_LINE;
+  msg += NEW_LINE;
 
-    const NEW_LINE = "\n";
-    const now = new Date();
+  group.ajza.forEach((juz) => {
+    msg +=
+      ('0' + (juz.index + 1)).slice(-2) +
+      ' ' +
+      getJuzIcon(juz) +
+      ' ' +
+      (juz.owner || '');
 
-    let msg = group.title;
-
-    msg += NEW_LINE;
-    msg += _getDateInArabic(now);
-    msg += NEW_LINE;
-    msg += NEW_LINE;
-
-    group.ajza.forEach(juz => {
-
-        msg += ("0" + (juz.index + 1)).slice(-2) + " " + getJuzIcon(juz) + " " + (juz.owner || "");
-
-        // if (juz.status === JUZ_STATUS.DONE) {
-        //   msg += " 👏";
-        // }
-
-        msg += NEW_LINE;
-
-    });
+    // if (juz.status === JUZ_STATUS.DONE) {
+    //   msg += " 👏";
+    // }
 
     msg += NEW_LINE;
-    msg += NEW_LINE;
+  });
 
-    if (group.targetDate) {
-        msg += "موعد تسليم الختمة: " + group.targetDate + ".";
-        msg += NEW_LINE;
-        msg += NEW_LINE;
-    }
+  msg += NEW_LINE;
+  msg += NEW_LINE;
 
-    msg += "رجاء حتلنة جزئكم عن طريق الرابط: " + group.getURL();
-
+  if (group.targetDate) {
+    msg += 'موعد تسليم الختمة: ' + group.targetDate + '.';
     msg += NEW_LINE;
     msg += NEW_LINE;
+  }
 
-    msg += "بارك الله بكم!";
+  msg += 'رجاء تحديث حالة جزئكم عن طريق الرابط: ' + group.getURL();
 
-    return msg;
+  msg += NEW_LINE;
+  msg += NEW_LINE;
+
+  msg += 'بارك الله بكم!';
+
+  return msg;
 }
 
-
 function statusMsg_SameTask(group) {
+  function getStatusIcon(isDone) {
+    return isDone ? '🟢' : '🔴';
+  }
 
-    function getStatusIcon(isDone) {
-        return isDone ? '🟢' : '🔴';
-    }
+  const NEW_LINE = '\n';
+  const now = new Date();
 
-    const NEW_LINE = "\n";
-    const now = new Date();
+  let msg = group.title;
 
-    let msg = group.title;
+  msg += NEW_LINE;
+  msg += _getDateInArabic(now);
+  msg += NEW_LINE;
+  msg += NEW_LINE;
 
+  msg += 'المهمّة الحاليّة: ' + group.task;
+  msg += NEW_LINE;
+  msg += NEW_LINE;
+
+  group.members.forEach((member) => {
+    msg += getStatusIcon(member.isTaskDone) + ' ' + member.name;
     msg += NEW_LINE;
-    msg += _getDateInArabic(now);
-    msg += NEW_LINE;
-    msg += NEW_LINE;
+  });
 
-    msg += "المهمّة الحاليّة: " + group.task;
-    msg += NEW_LINE;
-    msg += NEW_LINE;
+  msg += NEW_LINE;
 
-    group.members.forEach(member => {
-
-        msg += getStatusIcon(member.isTaskDone) + " " + member.name;
-        msg += NEW_LINE;
-
-    });
-
-    msg += NEW_LINE;
-
-    if (group.targetDate) {
-        msg += "موعد تسليم المهمّة: " + group.targetDate + ".";
-        msg += NEW_LINE;
-        msg += NEW_LINE;
-    }
-
-    msg += "رجاء حتلنة مهمّتكم عن طريق الرابط: " + group.getURL();
-
+  if (group.targetDate) {
+    msg += 'موعد تسليم المهمّة: ' + group.targetDate + '.';
     msg += NEW_LINE;
     msg += NEW_LINE;
+  }
 
-    msg += "بارك الله بكم!";
+  msg += 'رجاء تحديث حالة مهمّتكم عن طريق الرابط: ' + group.getURL();
 
-    return msg;
+  msg += NEW_LINE;
+  msg += NEW_LINE;
+
+  msg += 'بارك الله بكم!';
+
+  return msg;
 }
 
 let _StatusMessages = {};
 _StatusMessages[KHITMA_GROUP_TYPE.SEQUENTIAL] = statusMsg_Sequential;
 _StatusMessages[KHITMA_GROUP_TYPE.SAME_TASK] = statusMsg_SameTask;
-
 
 export const StatusMessageGenerators = _StatusMessages;
