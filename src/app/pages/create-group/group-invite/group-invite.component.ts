4424335f35e088558e9c4b24d85dea51eb64100d
Hasan Abo-Shally <HasanAboShally@gmail.com>
2022-04-02 12:44:44 +0300

    quick updates

--- src/app/pages/create-group/group-invite/group-invite.component.ts
+++ src/app/pages/create-group/group-invite/group-invite.component.ts
@@ -7,60 +7,60 @@ import { NativeShareService } from '../../../native-share.service';
 import { KhitmaGroupService } from '../../../khitma-group.service';
 import { CommonService } from '../../../service/common.service';
 
-
 @Component({
   selector: 'app-group-invite',
   templateUrl: './group-invite.component.html',
   styleUrls: ['./group-invite.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class GroupInviteComponent implements OnInit {
-
   group: KhitmaGroup;
   inviteLink: string;
   inviteMsg: string;
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private groupsApi: KhitmaGroupService,
     private alert: AlertService,
     private nativeShare: NativeShareService,
     private nativeApi: NativeApiService,
-    public common: CommonService) { }
+    public common: CommonService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params): void => {
+      const groupId = params.groupId;
 
-    this.route.params.subscribe(
-      (params: Params): void => {
-
-        const groupId = params.groupId;
-
-        this.groupsApi.getGroupDetailsOnce(groupId).subscribe((group: KhitmaGroup) => {
+      this.groupsApi
+        .getGroupDetailsOnce(groupId)
+        .subscribe((group: KhitmaGroup) => {
           this.group = group;
 
           this.inviteLink = this.groupsApi.getGroupURL(this.group.id);
 
-          this.inviteMsg = this.common.translation.gInvite?.join
-            + ' "' + this.group.title + '" '
-            + this.common.translation.gInvite?.link
-            + this.inviteLink + "\n"
-            + this.common.translation.gInvite?.bless;
-
+          this.inviteMsg =
+            this.common.translation.gInvite?.join +
+            ' "' +
+            this.group.title +
+            '" ' +
+            this.common.translation.gInvite?.link +
+            '" ' +
+            this.inviteLink +
+            '\n' +
+            this.common.translation.gInvite?.bless;
         });
-
-      });
-
+    });
   }
 
   msgCopied() {
-
     this.alert.show(this.common.translation.gInvite?.alert, 5000);
-
   }
 
   share() {
-
-    this.nativeApi.share(this.common.translation.gInvite?.share, this.inviteMsg, null);
-
+    this.nativeApi.share(
+      this.common.translation.gInvite?.share,
+      this.inviteMsg,
+      null
+    );
   }
-
 }
