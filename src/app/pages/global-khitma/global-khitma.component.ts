007efe621d4f3c2a6397bc7adfeb4abb6dae8624
Hasan Abo-Shally <HasanAboShally@gmail.com>
2022-04-08 10:16:01 +0300

    ramadan 2022

--- src/app/pages/global-khitma/global-khitma.component.ts
+++ src/app/pages/global-khitma/global-khitma.component.ts
@@ -7,7 +7,10 @@ import { LocalDatabaseService } from '../../local-database.service';
 import { map } from 'rxjs/operators';
 import { GoogleAnalyticsService } from 'ngx-google-analytics';
 import { MatDialog } from '@angular/material/dialog';
-import { ConfirmDialogComponent, ConfirmDialogModel } from '../../shared/confirm-dialog/confirm-dialog.component';
+import {
+  ConfirmDialogComponent,
+  ConfirmDialogModel,
+} from '../../shared/confirm-dialog/confirm-dialog.component';
 import { CommonService } from '../../service/common.service';
 
 @Component({
@@ -15,14 +18,15 @@ import { CommonService } from '../../service/common.service';
   templateUrl: './global-khitma.component.html',
   styleUrls: ['./global-khitma.component.scss'],
   // encapsulation: ViewEncapsulation.None
-
 })
 export class GlobalKhitmaComponent implements OnInit {
-
-  constructor(private groupsApi: KhitmaGroupService,
+  constructor(
+    private groupsApi: KhitmaGroupService,
     private localDB: LocalDatabaseService,
     private $gaService: GoogleAnalyticsService,
-    private dialog: MatDialog, public common: CommonService) { }
+    private dialog: MatDialog,
+    public common: CommonService
+  ) {}
 
   dayInRamadan = 0;
 
@@ -35,90 +39,87 @@ export class GlobalKhitmaComponent implements OnInit {
   totalAjzaCounter;
 
   ngOnInit(): void {
-
-
     function daysBetween(StartDate, EndDate) {
       // The number of milliseconds in all UTC days (no DST)
       const oneDay = 1000 * 60 * 60 * 24;
 
       // A day in UTC always lasts 24 hours (unlike in other time formats)
-      const start = Date.UTC(EndDate.getFullYear(), EndDate.getMonth(), EndDate.getDate());
-      const end = Date.UTC(StartDate.getFullYear(), StartDate.getMonth(), StartDate.getDate());
+      const start = Date.UTC(
+        EndDate.getFullYear(),
+        EndDate.getMonth(),
+        EndDate.getDate()
+      );
+      const end = Date.UTC(
+        StartDate.getFullYear(),
+        StartDate.getMonth(),
+        StartDate.getDate()
+      );
 
       // so it's safe to divide by 24 hours
       return (start - end) / oneDay;
     }
 
-    this.groupsApi.getGlobalKhitma("ramadan2021").get().subscribe((res: any) => {
+    this.groupsApi
+      .getGlobalKhitma('ramadan2021')
+      .get()
+      .subscribe((res: any) => {
+        let data = res.data();
 
-      let data = res.data();
+        let ramadanStartDate = new Date(data.startDate);
 
-      let ramadanStartDate = new Date(data.startDate);
+        let dayInRamadan = daysBetween(ramadanStartDate, new Date()) + 1;
 
-      let dayInRamadan = daysBetween(ramadanStartDate, new Date()) + 1;
+        // dayInRamadan = 10;
+        this.dayInRamadan = dayInRamadan; //> 0 ? dayInRamadan : 0;
 
-      // dayInRamadan = 10;
-      this.dayInRamadan = dayInRamadan;//> 0 ? dayInRamadan : 0;
+        this.myAjzaStatuses = this.localDB.getMyGlobalKhitmaAjza();
+        this.ajza = this.getAjzaArray(data.ajza);
 
-      this.myAjzaStatuses = this.localDB.getMyGlobalKhitmaAjza();
-      this.ajza = this.getAjzaArray(data.ajza);
-
-      this.totalAjzaCounter = data.totalAjzaCounter;
-    });
-
+        this.totalAjzaCounter = data.totalAjzaCounter;
+      });
   }
 
   getAjzaArray(ajzaObj) {
-
     let arr = [];
 
     for (const [key, value] of Object.entries(ajzaObj)) {
       arr.push({
         index: parseInt(key),
-        counter: value
+        counter: value,
       });
-
     }
 
     return arr;
-
   }
-
 
   submitJuz(juzIndex, isDone) {
-
     this.myAjzaStatuses[juzIndex] = isDone;
-    this.groupsApi.globalKhitmaUpdateJuz("ramadan2021", juzIndex, isDone);
+    this.groupsApi.globalKhitmaUpdateJuz('ramadan2021', juzIndex, isDone);
 
-    this.totalAjzaCounter += (isDone ? 1 : -1);
+    this.totalAjzaCounter += isDone ? 1 : -1;
     this.localDB.updateGlobalKhitmaJuz(juzIndex, isDone);
 
-
-    this.$gaService.event(isDone ? 'juz_done' : 'juz_undone', 'global_khitma', 'ramadan2021');
-
-
+    this.$gaService.event(
+      isDone ? 'juz_done' : 'juz_undone',
+      'global_khitma',
+      'ramadan2021'
+    );
   }
 
-
   resetKhitma() {
-
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       data: new ConfirmDialogModel(
         this.common.translation.gKhitma?.new,
-        this.common.translation.gKhitma?.bless),
-      maxWidth: "80%"
+        this.common.translation.gKhitma?.bless
+      ),
+      maxWidth: '80%',
     });
 
-    dialogRef.afterClosed().subscribe(dialogResult => {
-
+    dialogRef.afterClosed().subscribe((dialogResult) => {
       if (dialogResult) {
         this.localDB.resetMyGlobalKhitmaAjza();
         this.myAjzaStatuses = this.localDB.getMyGlobalKhitmaAjza();
       }
-
     });
-
-
   }
-
 }
